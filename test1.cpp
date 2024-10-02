d41d0b8a93aa4eb28f20ca8a5e0339407b938e55
chingyshan <vlad.ag070906@gmail.com>
2024-10-02 23:42:16 +0300

    J

--- test1.cpp
+++ test1.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    double x;
+    std::cin>>x;
+    if(x=1) {
+        std::cout << "Hello, World!" << std::endl;
+    } else {
+        std::cout<<"boombaks"<< std::endl;
+    }
     return 0;
 }
